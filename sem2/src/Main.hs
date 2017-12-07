05abde9cd773b002cba8f8992042b9718d8c4d36
Mansur Ziiatdinov <gltronred@gmail.com>
2017-12-07 14:24:02 +0300

    Transformers

--- sem2/src/Main.hs
+++ sem2/src/Main.hs
@@ -6,6 +6,12 @@ import Solution
 import Control.Monad
 import Data.List
 
+{- transformers -}
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Writer
+import Control.Monad.Trans.State
+import Control.Monad.Trans.Class
+
 {-
 :: f a -> f (a -> b) -> f b
 :: m a -> (a -> m b) -> m b
@@ -41,8 +47,79 @@ g listOfLists = do
 twice :: Monad m => m a -> m a
 twice act = act >> act
 
+readerFun :: Reader (Int, Double) Double
+readerFun = do
+  (x,y) <- ask
+  z <- asks fst
+  return $ y^z
+
+writerFun :: Writer String Int
+writerFun = do
+  tell "start"
+  tell "end"
+  return 42
+
+statefulFibo :: State (Int, (Integer, Integer)) Integer
+statefulFibo = do
+  (n, (x,y)) <- get
+  case n of
+    1 -> return x
+    2 -> return y
+    _ -> do
+      put (n-1, (y, x+y))
+      statefulFibo
+
+statefulFibo2 :: Int -> State (Integer, Integer) ()
+statefulFibo2 n = replicateM_ n $ do
+  (x,y) <- get
+  put (y, x+y)
+
+fibo2 n = fst $ execState (statefulFibo2 n) (0,1)
+
+
+{- RWS r w s
+   RWST r w s m
+e.g. RWST r w s IO
+e.g. RWST (Int,Double) () Int IO
+-}
+monadStack :: ReaderT (Int, Double) (StateT Int IO) ()
+monadStack = do
+  (x,y) <- ask
+  z <- lift get
+  lift $ put $ x + truncate y + z
+  lift $ lift $ print "Ololo!"
+
+
+newtype MySt s a = MySt { runMySt :: s -> (a, s) }
+newtype MyStT m s a = MyStT { runMyStT :: s -> m (a,s) }
+
+instance Functor (MySt s) where
+  fmap f (MySt g) = MySt $ \s -> let (a, s') = g s in (f a, s')
+
+instance Applicative (MySt s) where
+  pure x = MySt $ \s -> (x, s)
+  MySt sf <*> MySt sx = MySt $ \s -> let
+    (f, s') = sf s
+    (x, s'')= sx s'
+    in (f x, s'')
+
+instance Monad (MySt s) where
+  MySt sa >>= f = MySt $ \s -> let
+    (a, s') = sa s
+    MySt sb = f a
+    in sb s'
+
+get :: MySt s s
+get = MySt $ \s -> (s, s)
+
+put :: s -> MySt s ()
+put s = MySt $ \_ -> ((), s)
+
 main :: IO ()
 main = do
   putStr "x" >> putStrLn "y"
   putStrLn "hello world"
+  print $ runReader readerFun (5,6)
+  print $ runState statefulFibo (5, (0,1))
+  runStateT (runReaderT monadStack (5,6)) 2 >>= print
 
